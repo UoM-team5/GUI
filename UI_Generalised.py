2db7e46b43e3bb81e4cfee7fb086fc7eca7add7a
Gregos5 <78451671+Gregos5@users.noreply.github.com>
2022-10-29 16:20:01 +0100

    Update UI_Generalised.py

--- UI_Generalised.py
+++ UI_Generalised.py
@@ -3,16 +3,22 @@ from tkinter import ttk
 import serial
 import time
 
+# uncomment next line for arduino comm
+# arduino = serial.Serial(port='COM7', baudrate=115200, timeout=.1)
 
-arduino = serial.Serial(port='COM7', baudrate=115200, timeout=.1)
+# Send stuff to arduino through Serial
+def write(x):
+    # UNcomment 2 next lines for arduino comm
+    # arduino.write(bytes(x, 'utf-8'))
+    # time.sleep(0.05)
+    print(x)
+
+
 
 LARGE_FONT= ("Verdana", 13)
 BUTTON_FONT= ("Verdana", 10)
 
-# Send stuff to arduino through Serial
-def write(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
+
 
 
 # UI 
@@ -20,7 +26,7 @@ class Main(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        tk.Tk.wm_title(self, "Valve controller")
+        tk.Tk.wm_title(self, "Hardware control")
 
         container = tk.Frame(self)
         container.pack(side="top", fill = "both", expand = True)
@@ -29,7 +35,7 @@ class Main(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne):
+        for F in (StartPage, PageOne, PageTwo):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -48,45 +54,57 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text = "the Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = ttk.Button(self,
-         text="I want to control my valve",
-         font=BUTTON_FONT,
+        button1 = ttk.Button(self, text="I want to control my valve",
          command=lambda: controller.show_frame(PageOne))
         button1.pack()
+
+        button2 = ttk.Button(self, text="I want to control my stepper",
+         command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
 
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text = "Page 1 ", font=LARGE_FONT)
+        label = tk.Label(self, text = "control my valve UwU", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         
-        
-
-        button1 = ttk.Button(self,
-         text="Home",
-         font=BUTTON_FONT,
+        button1 = ttk.Button(self, text="Home",
          command=lambda: controller.show_frame(StartPage))
         button1.pack()
+        
+        button2 = ttk.Button(self, text="close my valve :(",
+         command=lambda: write("0"))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="open my valve :)",
+         command=lambda: write("1"))
+        button3.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text = "Pump me", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Home",
+         command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        MyText = "Type in the number of ml and press the button"
+        T = tk.Text(self, height = 1, width = 52)
+        T.insert(tk.END, MyText)
+        T.pack()
 
         entry = ttk.Entry(self)
         entry.pack()
 
-        button2 = ttk.Button(self,
-         text="send your text to Ard",
-         font=BUTTON_FONT,
+        button2 = ttk.Button(self, text="send your text to Ard",
          command=lambda: write(entry.get()))
-        button2.pack()
-        
-        button3 = ttk.Button(self, text="close my valve :(",
-         command=lambda: write("0"))
-        button3.pack()
-
-        button4 = ttk.Button(self, text="open my valve :)",
-         command=lambda: write("1"))
-        button4.pack()
-
-        
+        button2.pack()     
 
 
 
