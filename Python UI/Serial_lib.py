1f8b3ce775bb111c2ecaf991d7d9e58ce5164829
Gregos5 <78451671+Gregos5@users.noreply.github.com>
2023-02-02 12:03:03 +0000

    OOP
    uses classes

--- Python UI/Serial_lib.py
+++ Python UI/Serial_lib.py
@@ -1,6 +1,6 @@
 import serial
 import serial.tools.list_ports
-import time
+
 
 def ID_PORTS_AVAILABLE():
     devices = []
@@ -47,21 +47,21 @@ def SERIAL_READ_LINE(DEV):
 def DECODE_LINES(cmd_list):
     for i in range(0,len(cmd_list)):
         cmd_list[i] = cmd_list[i].decode('UTF-8').replace('\r\n','')
-        cmd_list[i] = decode_output(cmd_list[i])
+        cmd_list[i] = DECODE_LINE(cmd_list[i])
         print(cmd_list[i])
     return cmd_list
 
-def decode_output(command):
+def DECODE_LINE(command):
     if command[0]!="[" or command[-1]!="]":
-        return command #print("this is not valid: ", command)
+        return "1001 skip " + command #print("this is not valid: ", command)
     cmd_strip = command[1:-1]
     cmd_split = cmd_strip.split(" ", 2)
     senderID = cmd_split[0][3:]
     receiveID = cmd_split[1]
     PK = cmd_split[2]
-    return PACKAGE_DECODE(senderID, PK)
+    return DECODE_PACKAGE(senderID, PK)
 
-def PACKAGE_DECODE(senderID, PK):
+def DECODE_PACKAGE(senderID, PK):
     n_pk = int(PK[2:4])
     pk_split = PK.split(" ", n_pk)
     operator = pk_split[1]
@@ -92,6 +92,7 @@ def PACKAGE_DECODE(senderID, PK):
 
         case "V":
             ## LOG DATA HERE!!!
+            #buffer pop
             #get the command that you sent to arduino 
             senderID = senderID
             #log_valid_cmd()
@@ -153,4 +154,60 @@ def FLUSH_PORT(DEV):
             DEV[i].flushInput()
     except:
         pass
+
+
+# Functions to Generate Commands
+def valve(Valve_num, state):
+    command = "[sID1000 rID1001 PK2 V{} S{}]".format(Valve_num, state)
+    return command
+
+def pump(Pump_num, amount, dir=1):
+    # "[sID1000 rID1001 PK3 P2 m50.25 D1]"
+    command = "[sID1000 rID1001 PK3 P{} m{:.2f} D{}]".format(Pump_num, amount, dir)
+    return command
+
+def shutter(state, shutter_num=1):
+    # "[sID1000 rID1001 PK2 I1 S0]"
+    command = "[sID1000 rID1001 PK2 I{} S{}]".format(shutter_num, state)
+    return command
+
+def mixer(speed, time):
+    return
+
+def Sensor_read():
+    return "[sID1000 rID1001 PK1 R]"
+
+
+# FIFO Buffer
+class Buffer:
+    def __init__(self):
+        self.buffer = []
+
+    def IN(self, COMMAND):
+        if type(COMMAND) is str:
+            if len(self.buffer)<15:
+                self.buffer.append(COMMAND)
+            else:
+                print("BUFFER_FULL, try later")
+        if type(COMMAND) is list:
+            if len(self.buffer)<15:
+                for element in COMMAND:
+                    self.buffer.append(element)
+            else:
+                print("BUFFER_FULL, try later")
+
+    def OUT(self, DEV):
+        if len(self.buffer):
+            command = self.buffer[0]
+            messages = WRITE(DEV, command)
+            last = messages[len(messages)-1]
+            return last
+
+    def POP(self):
+        if len(self.buffer):
+            print("POPING")
+            self.buffer.pop(0)
+
+    def READ(self):
+        return self.buffer
 
--- Python UI/UI.py
+++ Python UI/UI.py
@@ -2,8 +2,6 @@ import time
 import tkinter as tk
 from tkinter import ttk
 import Serial_lib as com 
-import CMD as cmd
-
 
 LARGE_FONT= ("Verdana", 20)
 NORMAL_FONT= ("Verdana", 15)
@@ -12,12 +10,12 @@ SMALL_FONT= ("Verdana", 10)
 frame_styles = {"relief": "groove",
                 "bd": 3, "bg": "#f0a000",
                 "fg": "#073bb3", "font": ("Arial", 9, "bold")}
-                
-                
+                      
 #global variables 
 lastmessage = ""
-buffer = []
-recipe_1 = [cmd.valve(1,0), cmd.pump(1, 50.2), cmd.valve(1,1), cmd.valve(2,0)]
+messages = []
+buffer = com.Buffer()
+recipe_1 = [com.valve(1,0), com.pump(1, 5.2), com.valve(1,1)]
 
 def BEGIN_SERIAL(arduinos):
     global arduino_servo
@@ -65,6 +63,10 @@ class initialise(tk.Tk):
         button1 = ttk.Button(main_frame, text="Check connections",
          command=lambda: update_devices(label_arduino))
         button1.pack()
+
+        button2 = ttk.Button(self, text="ask Details",
+         command=lambda: com.WRITE(arduino_servo, "[sID1000 rID1001 PK1 DETAIL]"))
+        button2.pack()  
 
         label_arduino = tk.Label(main_frame, text ="there is {} arduinos connected".format(len(arduinos)), font=NORMAL_FONT)
         label_arduino.pack()
@@ -128,56 +130,53 @@ class Main(tk.Tk):
     def Quit_application(self):
         self.destroy()
 
-
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent, bg = frame_styles["bg"])
-        title = tk.Label(self, text = "Home", font=LARGE_FONT)
+        title = tk.Label(self, text = "Home", font=LARGE_FONT, bg = frame_styles["bg"])
         title.pack(pady=10,padx=10)
+
+        button3 = ttk.Button(self, text="ask Details",
+         command=lambda: buffer.IN("[sID1000 rID1001 PK1 DETAIL]"))
+        button3.pack()  
 
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent, bg = frame_styles["bg"])
-        label = tk.Label(self, text = "control my valve", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        label = tk.Label(self, text = "Valve Control", font=LARGE_FONT, bg = frame_styles["bg"])
+        label.grid(row=0, column= 2, columnspan=3)
         
-        
+        def entry_block(text, pos):
+            row,col = pos
+            T = tk.Label(self, text = text, font=SMALL_FONT, bg = frame_styles["bg"])
+            entry = ttk.Entry(self, width=8)
+            T.grid(row=row, column=col, sticky='E')
+            entry.grid(row=row, column=col+1, sticky='W')
+            return entry
 
-        button2 = ttk.Button(self, text="close my valve :(",
-         command=lambda: cmd.BUFFER_IN(buffer, cmd.valve(1,0)))
-        button2.pack()
+        valve_number = entry_block("Valve number: ", [1,1])
 
-        button3 = ttk.Button(self, text="open my valve :)",
-         command=lambda: cmd.BUFFER_IN(buffer, cmd.valve(1,1)))
-        button3.pack()
+        button2 = ttk.Button(self, text="close",
+         command=lambda: buffer.IN(com.valve(int(valve_number.get()),0)))
+        button2.grid(column=2, sticky='w')
+
+        button3 = ttk.Button(self, text="open",
+         command=lambda: buffer.IN(com.valve(int(valve_number.get()),1)))
+        button3.grid(column=2, sticky='w')
 
 class PageTwo(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent, bg = frame_styles["bg"])
-        label = tk.Label(self, text = "Pump me", font=LARGE_FONT)
+        label = tk.Label(self, text = "Pump Control", font=LARGE_FONT, bg = frame_styles["bg"])
         label.pack(pady=10,padx=10)
-
-    
-
-        MyText = "Type in the number of ml and press the button"
-        T = tk.Text(self, height = 1, width = 52)
-        T.insert(tk.END, MyText)
-        T.pack()
-
-        entry0 = ttk.Entry(self)
-        entry0.pack()
-
-        button2 = ttk.Button(self, text="send your text to Ard",
-         command=lambda: com.WRITE(arduino_servo, cmd.pump(1, float(entry0.get()))))
-        button2.pack()     
 
 class PageThree(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent, bg = frame_styles["bg"])
-        label = tk.Label(self, text = "Pump from Ra or Rb", font=LARGE_FONT)
+        label = tk.Label(self, text = "Multi-Pump Control", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
 
@@ -186,32 +185,6 @@ class PageThree(tk.Frame):
         T1.insert(tk.END, MyText)
         T1.pack()
 
-        entry0 = ttk.Entry(self)
-        entry0.pack()
-
-        button2 = ttk.Button(self, text="send your text to Ard",
-         command=lambda: print(com.WRITE(arduino_servo, cmd.pump(1, float(entry0.get())))))
-        button2.pack() 
-
-        MyText2 = "R B: Type in the number of ml"
-        T2 = tk.Text(self, height = 1, width = 52)
-        T2.insert(tk.END, MyText2)
-        T2.pack()
-
-        entry1 = ttk.Entry(self)
-        entry1.pack()
-
-        button3 = ttk.Button(self, text="send your text to Ard",
-         command=lambda: com.WRITE(arduino_servo, cmd.pump(1, float(entry1.get()))))
-        button3.pack() 
-
-        button4 = ttk.Button(self, text="Is there Liquid?",
-            command=lambda: update_label(my_label, "yes"))
-        button4.pack() 
-
-        my_label = tk.Label(self, text = "no liquid", font=SMALL_FONT)
-        my_label.pack()
-
 class PageFour(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -219,13 +192,13 @@ class PageFour(tk.Frame):
         label = tk.Label(self, text = "Recipe", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        MyText = "[cmd.valve(1,0), cmd.pump(1, 50.2), cmd.valve(1,1), cmd.valve(2,0)]"
+        MyText = str(recipe_1)
         T = tk.Text(self, height = 3, width = 52)
         T.insert(tk.END, MyText)
         T.pack()
 
         button2 = ttk.Button(self, text="send your recipe to Ard",
-         command=lambda: cmd.BUFFER_IN(buffer, recipe_1))
+         command=lambda: buffer.IN(recipe_1))
         button2.pack()     
 
 class PageFive(tk.Frame):
@@ -233,7 +206,7 @@ class PageFive(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent, bg = frame_styles["bg"])
         
-        label = tk.Label(self, text = "Run iterative loop", font=NORMAL_FONT, bg = frame_styles["bg"])
+        label = tk.Label(self, text = "Iterative Sequence", font=LARGE_FONT, bg = frame_styles["bg"])
         label.grid(row=0, column= 0, columnspan=3)
 
         def entry_block(text, pos):
@@ -244,38 +217,35 @@ class PageFive(tk.Frame):
             entry.grid(row=row, column=col+1, sticky='W')
             return entry
 
-        e_Ra_ml = entry_block("Ra:", [1,0])
-        e_Ra_step = entry_block("step:",[1,2])
+        e_Ra_ml = entry_block("Liquid A:", [1,0])
+        e_Ra_step = entry_block("step A:",[1,2])
 
-        e_Rb_ml = entry_block("Rb:",[2,0])
-        e_Rb_step = entry_block("step:",[2,2])
-        e_count = entry_block("loop count:",[3,0])
+        e_Rb_ml = entry_block("Liquid B:",[2,0])
+        e_Rb_step = entry_block("step B:",[2,2])
+        e_count = entry_block("iterations:",[3,0])
 
-        button1 = ttk.Button(self, text="send", command=lambda: send_command())
+        button1 = ttk.Button(self, text="Start", command=lambda: send_command())
         button1.grid(row=4, column=2) 
 
         def send_command():
             cmd_list = []
-            Ra_ml = e_Ra_ml.get()
+            Ra_ml_init = e_Ra_ml.get()
             Ra_step = e_Ra_step.get()
-            Rb_ml = e_Rb_ml.get()
+            Rb_ml_init = e_Rb_ml.get()
             Rb_step = e_Rb_step.get()
             count = int(e_count.get())
 
             for i in range(count):
-                if Ra_ml!="" and Ra_step!="":
-                    Ra_ml = float(Ra_ml) + float(Ra_step)*i
-                    cmd_list.append(cmd.pump(1, Ra_ml))
-                if Rb_ml!="" and Rb_step!="":
-                    Ra_ml = float(Ra_ml) + float(Rb_step)*i
-                    cmd_list.append(cmd.pump(1, float(Rb_ml)))
+                if Ra_ml_init!="" and Ra_step!="":
+                    Ra_ml = float(Ra_ml_init) + float(Ra_step)*i
+                    cmd_list.append(com.pump(1, Ra_ml))
+                if Rb_ml_init!="" and Rb_step!="":
+                    Rb_ml = float(Rb_ml_init) + float(Rb_step)*i
+                    cmd_list.append(com.pump(1, float(Rb_ml)))
 
-            cmd.BUFFER_IN(buffer, cmd_list)
+            buffer.IN(cmd_list)
             return
-        
-        
-        
-
+         
 class OpenNewWindow(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -297,26 +267,65 @@ class OpenNewWindow(tk.Tk):
         label1 = tk.Label(frame1, font=("Verdana", 20), text="Plotting Page")
         label1.pack(side="top")
 
-# LoopEvent1 = if cmd waiting in Buffer => read lines; Frequency = 1/200ms 
-# LoopEvent2: empty buffer: if receive FREE => BUFFER_OUT fifo = send next command
-def task():
+
+def event_handler(MESSAGES):
     global lastmessage
-    global buffer
+    global buffer 
+    for MESSAGE in MESSAGES:
+        match MESSAGE[5]:
+            case "E":
+                print('REPEAT')
+
+            case "V":
+                buffer.POP()
+                print("left in the buffer:", buffer.READ())
+
+            case "F":
+                if len(buffer.READ())>0:
+                    lastmessage = buffer.OUT(arduino_servo)
+                    
+            case _:
+                pass
+    
+    return 
+
+
+def init_task():
+    global lastmessage
+    global messages
     if (arduino_servo.inWaiting() > 0):
         messages = com.SERIAL_READ_LINE(arduino_servo)
         lastmessage = messages[len(messages)-1]
-        print("\nthe last message is ==>", lastmessage, "\n")
-    if ("FREE" in lastmessage and len(buffer)>0):
-        buffer,lastmessage = cmd.BUFFER_OUT(arduino_servo, buffer)
+        print("the last messge is: ", lastmessage)
+        messages=[lastmessage]
+    init.after(200, init_task)
+    time.sleep(0.01)
+#initialisation 
+#init = initialise()
+#init.after(200, init_task)
+#init.mainloop()
+
+def task():
+    global lastmessage
+    global messages
+    global buffer
+
+    
+    if (arduino_servo.inWaiting() > 0):
+        messages = com.SERIAL_READ_LINE(arduino_servo)
+        lastmessage = messages[len(messages)-1]
+        event_handler(messages)
+        messages=[lastmessage]
+
+     
+    if len(messages)>0 and len(buffer.READ())>0:
+        event_handler(messages)
+        lastmessage = messages[len(messages)-1]
+        messages=[]
     
 
     gui.after(100, task)
     time.sleep(0.01)
-
-#initialisation 
-# init = initialise()
-# init.mainloop()
-
 #GUI
 gui = Main()
 gui.after(100,task)
